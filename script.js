907311462eb3d76e17dc93566ec2f5a12fd260dd
Namunabhattarai <nnamuna999@gmail.com>
2020-10-08 22:29:02 +0545

    my truth n dare project

---
+++ script.js
@@ -0,0 +1,28 @@
+// var truth=[
+//     " What is the biggest lie you have ever told?",
+//     "What’s the last movie that made you cry?",
+//     "What’s the biggest secret you’ve kept from your parents?",
+//     "Have you ever fallen out of the bed, at midnight?",
+//     "Who loves you more among your parents and why?",
+//     "If You Could Make One Wish Right This Second, What Would It Be?",
+//     "What Is The Most Embarrassing Picture Of You?"
+// ];
+
+// var dare=[
+//     "Give someone your phone and let them send one text to anyone in your contacts.",
+//     "Make every person in the group smile, keep going until everyone has cracked a smiled.",
+//     "Eat one teaspoon of the spiciest thing you have in the kitchen.",
+//     "Unlock your phone and hand it over to your partner."
+// ];
+
+// var container=document.getElementById('container');
+// var truth=document.getElementById('truth');
+// var dare=document.getElementById('rare');
+
+// truth.addEventListener("click",function(){
+//     var randomTruth=truth[Math.floor(Math.random()*truth.length)];
+//     test.innerHtml="namuna;"
+
+    
+    
+// })
